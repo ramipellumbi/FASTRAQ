84573c17e1d126a56521ae3b157ee39fb63dd4f9
Rami Pellumbi <98100379+ramipellumbi@users.noreply.github.com>
2024-05-03 12:39:46 -0400

    Organize exports

--- src/schemas/articles/entities.types.ts
+++ src/schemas/articles/entities.types.ts
@@ -2,5 +2,5 @@ import { Static } from '@sinclair/typebox';
 
 import { Article, Tag } from './entities';
 
-export type TTag = Static<typeof Tag>;
 export type TArticle = Static<typeof Article>;
+export type TTag = Static<typeof Tag>;
