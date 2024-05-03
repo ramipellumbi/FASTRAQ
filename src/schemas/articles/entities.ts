0fa3fa9244a301b2817c41201928a9543e6a6011
Rami Pellumbi <98100379+ramipellumbi@users.noreply.github.com>
2024-05-03 12:38:11 -0400

    Name schemas accordingly

--- src/schemas/articles/entities.ts
+++ src/schemas/articles/entities.ts
@@ -5,7 +5,7 @@ import { Schema, StringEnum } from '../util';
 
 export const Tag = StringEnum('Tag', TAGS);
 
-export const Article = Schema('IArticle', {
+export const Article = Schema('Article', {
   author: Type.String(),
   title: Type.String(),
   content: Type.String(),
